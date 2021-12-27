530d5713ad95d4dbcc078e5e7500cefe8aac5e07
mth9406 <mth9406@naver.com>
2021-12-27 19:08:19 +0900

    ...

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author= 'SUNGWOO HUR',
     author_email= 'hursungwoo@postech.ac.kr',
     url= 'https://github.com/mth9406/Entropy-Fuzzy-Support-Vector-Machine.git',
-    install_requires= ['cvxopt == 1.2.7'],
+    install_requires= ['cvxopt == 1.2.7', 'sklearn >= 0.23.1', 'numpy >= 1.19.5'],
+    packages = ['efsvm'],
     zip_safe = False
 )
