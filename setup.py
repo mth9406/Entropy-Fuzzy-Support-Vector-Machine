668297da13e47337417b1b7090ef7e97c511163f
mth9406 <mth9406@naver.com>
2021-12-27 18:57:10 +0900

    modified setup.py, requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name= 'EFSVM',
+    name= 'efsvm',
     version= '0.0.1',
     description= 'Implemenation of EFSVM',
     author= 'SUNGWOO HUR',
     author_email= 'hursungwoo@postech.ac.kr',
     url= 'https://github.com/mth9406/Entropy-Fuzzy-Support-Vector-Machine.git',
-    install_requires= ['cvxopt == 1.2.7'],
+    install_requires= ['cvxopt == 1.2.7','sklearn == 0.23.1'],
     zip_safe = False
 )
